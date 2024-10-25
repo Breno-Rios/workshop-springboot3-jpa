869c1d7737e7d62563dfacd1bbe03614126f0dfb
Breno-Rios <brenooliveirarios@gmail.com>
2024-10-24 23:39:37 -0300

    Payment, one-to-one association

--- src/main/java/com/rios/course/config/TestConfig.java
+++ src/main/java/com/rios/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rios.course.entities.Category;
 import com.rios.course.entities.Order;
 import com.rios.course.entities.OrderItem;
+import com.rios.course.entities.Payment;
 import com.rios.course.entities.Product;
 import com.rios.course.entities.User;
 import com.rios.course.entities.enums.OrderStatus;
@@ -80,7 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
