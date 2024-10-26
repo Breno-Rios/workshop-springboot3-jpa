91878cac5ce9ce3ca723154545ca786746a59a83
Breno-Rios <brenooliveirarios@gmail.com>
2024-10-26 15:26:45 -0300

    Exception handling - update

--- src/main/java/com/rios/course/services/UserServices.java
+++ src/main/java/com/rios/course/services/UserServices.java
@@ -13,6 +13,8 @@ import com.rios.course.entities.repositories.UserRepository;
 import com.rios.course.services.exceptions.DatabaseException;
 import com.rios.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserServices {
 
@@ -49,10 +51,13 @@ public class UserServices {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
